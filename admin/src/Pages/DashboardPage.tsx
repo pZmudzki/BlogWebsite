e39eec940ee3c1470752f5109e3623b710a0d697
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-02-11 02:58:54 +0100

    create latest post card

--- admin/src/Pages/DashboardPage.tsx
+++ admin/src/Pages/DashboardPage.tsx
@@ -1,5 +1,6 @@
 import CreatePostCard from "@/components/HomePageComponents/CreatePostCard";
 import NewCommentsCard from "@/components/HomePageComponents/NewCommentsCard";
+import LatestPostCard from "@/components/HomePageComponents/LatestPostCard";
 export default function DashboardPage() {
   return (
     <main className="grid grid-cols-3 grid-rows-3 gap-3">
@@ -8,6 +9,9 @@ export default function DashboardPage() {
       </article>
       <article className="">
         <NewCommentsCard />
+      </article>
+      <article className="">
+        <LatestPostCard />
       </article>
     </main>
   );
--- admin/src/components/HomePageComponents/LatestPostCard.tsx
+++ admin/src/components/HomePageComponents/LatestPostCard.tsx
@@ -6,19 +6,49 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import { Button } from "../ui/button";
+import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
+
+const post = {
+  id: "uwhr892hrc892",
+  title: "PYTANIA",
+  content:
+    "Słyszę Twój krok w przedpokoju, Czuję już oddech na plecach, Strach obezwładnia mój umysł Odwaga schowała głowę już w piach I czekam na słowo lub zdanie, Które rozwieje codzienną niepewność Czy miłe czy nie będzie to spotkanie Czy wieczór we łzach czy pozorna radość",
+  createdAt: new Date().toLocaleString(),
+  views: 323,
+  archived: false,
+};
 
 export default function LatestPostCard() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle>Post</CardTitle>
+        <CardDescription>Najnowszy post</CardDescription>
       </CardHeader>
       <CardContent>
-        <p>Card Content</p>
+        <div className="flex flex-col gap-5">
+          <div className="flex justify-between">
+            <h1 className="font-bold text-2xl">{post.title}</h1>
+            <p>{post.createdAt}</p>
+          </div>
+          <p className="text-2xl">{post.content}</p>
+          <div className="flex justify-between">
+            <img
+              className=" max-h-[500px]	"
+              src="https://monalizabezramy.com/wp-content/uploads/elementor/thumbs/wiersze1-p6kpbup0b6vwyv9fea9nspz2fpu0eu07mmspi81t0o.jpg"
+            />
+            <p className="ml-auto">Wyświetlenia: {post.views.toString()}</p>
+          </div>
+        </div>
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <Link to="/dashboard/posts">
+          <Button>
+            Zarządzaj postami <ChevronDoubleRightIcon className="h-6 w-6" />
+          </Button>
+        </Link>
       </CardFooter>
     </Card>
   );
