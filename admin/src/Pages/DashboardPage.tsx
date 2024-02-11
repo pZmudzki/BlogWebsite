be01df038398de32628acb29e6b9bb455fb0c2d0
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-02-11 02:35:17 +0100

    create new comments card

--- admin/src/Pages/DashboardPage.tsx
+++ admin/src/Pages/DashboardPage.tsx
@@ -1,10 +1,13 @@
 import CreatePostCard from "@/components/HomePageComponents/CreatePostCard";
-
+import NewCommentsCard from "@/components/HomePageComponents/NewCommentsCard";
 export default function DashboardPage() {
   return (
-    <main className="grid-cols-2 grid-rows-3">
-      <article className="">
+    <main className="grid grid-cols-3 grid-rows-3 gap-3">
+      <article className="col-span-1">
         <CreatePostCard />
+      </article>
+      <article className="">
+        <NewCommentsCard />
       </article>
     </main>
   );
--- admin/src/components/HomePageComponents/NewCommentsCard.tsx
+++ admin/src/components/HomePageComponents/NewCommentsCard.tsx
@@ -6,18 +6,78 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Link } from "react-router-dom";
+import { ChevronDoubleRightIcon } from "@heroicons/react/24/solid";
+import { Button } from "../ui/button";
+
+const placeholderComments = [
+  {
+    id: "gfoqwyencue901cum",
+    postId: "d2h189cem1h9r1c",
+    name: "Test",
+    email: "test@test.com",
+    comment: "Fajny post jest super i generalnie jest w pyte bo jest git",
+    createdAt: new Date().toLocaleString(),
+    read: false,
+    accepted: false,
+  },
+  {
+    id: "rc324rc325c2",
+    postId: "d2h189cem1h9r1c",
+    name: "Test",
+    email: "test@test.com",
+    comment: "Fajny post jest super i generalnie jest w pyte bo jest git",
+    createdAt: new Date().toLocaleString(),
+    read: false,
+    accepted: false,
+  },
+  {
+    id: "3c52veyb5sy5yv",
+    postId: "d2h189cem1h9r1c",
+    name: "Test",
+    email: "test@test.com",
+    comment: "Fajny post jest super i generalnie jest w pyte bo jest git",
+    createdAt: new Date().toLocaleString(),
+    read: false,
+    accepted: false,
+  },
+  {
+    id: "v4a5ca546baev6e4",
+    postId: "d2h189cem1h9r1c",
+    name: "Test",
+    email: "test@test.com",
+    comment: "Fajny post jest super i generalnie jest w pyte bo jest git",
+    createdAt: new Date().toLocaleString(),
+    read: false,
+    accepted: false,
+  },
+];
+
 export default function NewCommentsCard() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
+        <CardTitle>Komentarze</CardTitle>
+        <CardDescription>Najnowsze komentarze użytkowników</CardDescription>
       </CardHeader>
-      <CardContent>
-        <p>Card Content</p>
+      <CardContent className="flex flex-col gap-3">
+        {placeholderComments.map((comment) => (
+          <div key={comment.id}>
+            <div className="flex justify-between">
+              <p>{comment.name}</p>
+              <p>{comment.createdAt}</p>
+            </div>
+            <p>{comment.comment}</p>
+          </div>
+        ))}
       </CardContent>
       <CardFooter>
-        <p>Card Footer</p>
+        <Link to="/dashboard/comments">
+          <Button>
+            Zobacz wszystkie komentarze{" "}
+            <ChevronDoubleRightIcon className="h-6 w-6" />
+          </Button>
+        </Link>
       </CardFooter>
     </Card>
   );
