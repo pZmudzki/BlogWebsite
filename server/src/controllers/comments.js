94939ed451c25ab3676b9dc61e56b3840ad5ccf3
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-02-20 21:41:24 +0100

    create update comment endpoint

--- server/src/controllers/comments.js
+++ server/src/controllers/comments.js
@@ -23,4 +23,18 @@ const getComments = async (req, res) => {
   }
 };
 
-module.exports = { addComment, getComments };
+const updateComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const comment = req.body;
+    const updatedComment = await Comment.findByIdAndUpdate(id, comment, {
+      new: true,
+    });
+    res.status(200).json(updatedComment);
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json({ error: "Błąd w aktualizacji komentarza" });
+  }
+};
+
+module.exports = { addComment, getComments, updateComment };
--- server/src/routes/comments.js
+++ server/src/routes/comments.js
@@ -2,9 +2,14 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middlewares/auth");
 
-const { addComment, getComments } = require("../controllers/comments");
+const {
+  addComment,
+  getComments,
+  updateComment,
+} = require("../controllers/comments");
 
 router.post("", addComment);
-router.get("/postId", getComments);
+router.get("/:postId", getComments);
+router.put("/:id", auth, updateComment);
 
 module.exports = router;
