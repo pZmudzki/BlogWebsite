0b79d99180fc0c4a6fe26cc00463a98d69795c49
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-02-22 19:45:47 +0100

    connect a search state to posts page

--- admin/src/Pages/PostsPage.tsx
+++ admin/src/Pages/PostsPage.tsx
@@ -1,3 +1,11 @@
+import { useSelector } from "react-redux";
+import { type RootState } from "@/store/store";
+import { useEffect } from "react";
+
 export default function PostsPage() {
+  const searchInput = useSelector<RootState>((state) => state.search.input);
+  useEffect(() => {
+    console.log(searchInput);
+  }, [searchInput]);
   return <div>Posts</div>;
 }
